46926cf9f5a089229e00ad9e7ca569a2021d56fe
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-11-21 18:20:14 +0100

    jhb

--- login/error.php
+++ login/error.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>error</title>
 </head>
+
 <body>
     <header>
         DATAlogin
     </header>
 
     <div class="error">
-        Non hai i diritti necessari per accedere.
+        Non hai i permessi per accedere ad db.
     </div>
 </body>
 </html>
