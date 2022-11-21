0b5d88ff35035825c3e912e82f695c8c9bc20012
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-11-21 21:07:52 +0100

    ouv

--- login/datalogin.php
+++ login/datalogin.php
@@ -4,12 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">	
-    <?php
-	    if($_SERVER['REMOTE_ADDR'] != "::1"){
-		    echo "<link rel='stylesheet' href='css/style.css'>";
-        }
-    ?>
-    
+    <link rel='stylesheet' href='css/style.css'>
     <title><?php echo basename(getcwd()) ?></title>
 </head>
 <body>
