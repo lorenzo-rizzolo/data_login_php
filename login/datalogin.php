ad2f060cfb66cbde1caf9781cd1d34da0560c57e
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2022-11-22 16:06:43 +0100

    kjhv

--- login/datalogin.php
+++ login/datalogin.php
@@ -8,6 +8,16 @@
     <title><?php echo basename(getcwd()) ?></title>
 </head>
 <body>
+<script>
+    function show(){
+        document.getElementById('pul_menu').innerHTML = "<button onclick='not_show()'>Nascondi Men&ugrave;</button>";
+        document.getElementById('opt').style.display = "inline-block";
+    }
+    function not_show(){
+        document.getElementById('pul_menu').innerHTML = "<button onclick='show()'>Mostra Men&ugrave;</button>";
+        document.getElementById('opt').style.display = "none";
+    }
+</script>
 
 <?php
     session_start();
@@ -24,48 +34,54 @@
     </header>
     <?php include "../data_login.php"; ?>
 
-    <div class="cerca">
-        <h4>Cerca Utente</h4>
-        <form method="post">
-            <input type="text" name="cerca" placeholder="cerca nome...">
-            <input type="submit" value="cerca" name="invia">
-        </form>
+    <div id="pul_menu">
+        <button onclick="show()">Mostra Men&ugrave;</button>
     </div>
-    <div class="cerca">
-        <h4>Nuovo utente semplice</h4>
-        <form method="post">
-            <input type="text" name="nome" placeholder="nome">
-            <input type="text" name="pas" placeholder="password">
-            <input type="submit" value="crea" name="nuovo">
-        </form>
-        <?php
-            if(isset($_POST['nuovo'])){
-                $name = $_POST['nome'];
-                $passw = $_POST['pas'];
-                if($name!="" && $passw!=""){
-                    add_user_basic($name, $passw);
+
+    <div id="opt">
+        <div class="cerca">
+            <h4>Cerca Utente</h4>
+            <form method="post">
+                <input type="text" name="cerca" placeholder="cerca nome...">
+                <input type="submit" value="cerca" name="invia">
+            </form>
+        </div>
+        <div class="cerca">
+            <h4>Nuovo utente semplice</h4>
+            <form method="post">
+                <input type="text" name="nome" placeholder="nome">
+                <input type="text" name="pas" placeholder="password">
+                <input type="submit" value="crea" name="nuovo">
+            </form>
+            <?php
+                if(isset($_POST['nuovo'])){
+                    $name = $_POST['nome'];
+                    $passw = $_POST['pas'];
+                    if($name!="" && $passw!=""){
+                        add_user_basic($name, $passw);
+                    }
                 }
-            }
-        ?>
-    </div>
-    <div class="cerca">
-        <h4>Elimina utente</h4>
-        <form method="post">
-            <input type="text" name="nomeus" placeholder="nome">
-            <input type="text" name="passw" placeholder="password">
-            <input type="submit" value="elimina" name="elimina">
-        </form>
-        <?php
-            if(isset($_POST['elimina'])){
-                $name = $_POST['nomeus'];
-                $passwd = $_POST['passw'];
-                if($name!="" && $passwd!=""){
-                    //$passw = substr($passw, 0, 1);
-                    //echo $name.$passwd." eliminato ";
-                    del_user_basic($name, $passwd);
+            ?>
+        </div>
+        <div class="cerca">
+            <h4>Elimina utente</h4>
+            <form method="post">
+                <input type="text" name="nomeus" placeholder="nome">
+                <input type="text" name="passw" placeholder="password">
+                <input type="submit" value="elimina" name="elimina">
+            </form>
+            <?php
+                if(isset($_POST['elimina'])){
+                    $name = $_POST['nomeus'];
+                    $passwd = $_POST['passw'];
+                    if($name!="" && $passwd!=""){
+                        //$passw = substr($passw, 0, 1);
+                        //echo $name.$passwd." eliminato ";
+                        del_user_basic($name, $passwd);
+                    }
                 }
-            }
-        ?>
+            ?>
+        </div>
     </div>
     <br>
     <?php
--- login/var.php
+++ login/var.php
@@ -1,3 +1,5 @@
 <?php
+
 $password = "password";
+
 ?>
